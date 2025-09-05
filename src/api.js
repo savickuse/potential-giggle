ff6dc56bb836bcf0fedd80dc9007ae933afb7966
savickuse <savickuse@users.noreply.github.com>
2025-09-05 20:44:04 +0300

    docs(utils): optimize database query

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 10
 module.exports = API;
+
+
+// API module for GiggleAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 15
+module.exports = API;
