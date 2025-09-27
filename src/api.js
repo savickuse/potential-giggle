7b3b392934c9bf010c97e8c23f117db64ee0625b
savickuse <savickuse@users.noreply.github.com>
2025-09-27 11:32:14 +0300

    add: improve code structure

--- src/api.js
+++ src/api.js
@@ -212,3 +212,30 @@ class API {
 
 // Update 60
 module.exports = API;
+
+
+// API module for GiggleAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 61
+module.exports = API;
