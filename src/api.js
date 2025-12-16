a2036362051ee55815a433d8f34d64c599c80a76
savickuse <savickuse@users.noreply.github.com>
2025-12-16 16:04:10 +0200

    style(ui): fix null pointer exception

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for GiggleAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 3
+module.exports = API;
