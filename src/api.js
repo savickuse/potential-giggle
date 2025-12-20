fc7a78e1becc60fdd571f02202d880763640a295
savickuse <savickuse@users.noreply.github.com>
2025-12-20 21:09:37 +0200

    docs: code refactoring

--- src/api.js
+++ src/api.js
@@ -185,3 +185,30 @@ class API {
 
 // Update 45
 module.exports = API;
+
+
+// API module for GiggleAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 60
+module.exports = API;
